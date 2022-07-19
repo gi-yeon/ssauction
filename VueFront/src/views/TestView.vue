1045aa430102899b9a57508de95f524707a3fe3e
giyeon3145 <giyeon3145@gmail.com>
2022-07-19 17:15:53 +0900

    axios

---
+++ VueFront/src/views/TestView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container-fluid mt-4">
+    <div class="row align-items-center">
+      <div class="col-lg-4 col-sm-8">
+        <temp></temp>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Temp from "./components/TestComponents.vue";
+export default {
+  name: "TestView",
+  components: {
+    Temp
+  },
+};
+</script>
