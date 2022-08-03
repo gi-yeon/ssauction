379ab1d2cc66682a8816f658067f88255fead5af
JiHyeon Jeong <gus4805@naver.com>
2022-08-03 15:24:53 +0900

    [FIX] for new merge

--- ssauction-front/src/store/modules/user.js
+++ ssauction-front/src/store/modules/user.js
@@ -29,136 +29,18 @@ const getters = {
   userGrade: (state) => {
     return `${state.loginUser.grade}`;
   },
-  accessToken: (state) => {
-    return `${state.accessToken}`;
-  },
-  refreshToken: (state) => {
-    return `${state.refreshToken}`;
-  },
+
   // userInfo: (state) => {
   //   return `'닉네임' : ${state.nickname}, '유저번호' : ${state.userNo}, '등급' : ${state.grade}`;
   // },
 };
 
 const actions = {
-  //로그인
-  userLogin({ commit }, loginInfo) {
-    axios.post("/users/login", JSON.stringify(loginInfo)).then((res) => {
-      //success 메시지가 떴다면
-      if (res.data.message === "success") {
-        console.log(res);
-        commit('USER_LOGIN', res);
-        commit('SAVE_LOGIN_INFO');
-        alert("로그인 되었습니다!");
-        router.push('/'); //홈으로 이동
-      }
-      //success 메시지가 뜨지 않았다면
-      else {
-        alert("아이디 또는 비밀번호가 일치하지 않습니다.");
-        router.push({ name: 'userLogin' });
-      }
-    })
-  },
-
-  //로그아웃
-  userLogout({ commit }) {
-    axios.post("/users/logout").then((res) => {
-      commit('USER_LOGOUT')
-      commit('RESET_LOGIN_INFO')
-      console.log(res.data)
-
-      alert("로그아웃되었습니다. 다음에 또 만나요 ^____^")
-      router.push('/'); //홈으로 이동
-
-    })
-
-  },
-
-  //로그인 정보 가져오기
-  getLoginInfo({ commit }) {
-    commit('GET_LOGIN_INFO')
-  },
-
-  //토큰 재발급
-  refreshToken() {
-    axios
-      .post("/users/refresh", JSON.stringify(state.loginUser.userNo))
-      .then((res) => {
-        console.log(res);
-        if (res.data.message === "success") {
-          alert("refresh성공");
-        }
-      });
-  },
-
-
-  getNickname({ commit }, value) {
-    commit(USER.SET_NICKNAME, value);
-  },
-  getGrade({ commit }, value) {
-    commit(USER.SET_GRADE, value);
-  },
-  getUserNo({ commit }, value) {
-    commit(USER.SET_USERNO, value);
-  },
+ 
 };
 
 const mutations = {
 
-  [USER.SET_NICKNAME](state, value) {
-    state.nickname = value;
-  },
-  [USER.SET_GRADE](state, value) {
-    state.grade = value;
-  },
-  [USER.SET_USERNO](state, value) {
-    state.userNo = value;
-  },
-  //로그인
-  USER_LOGIN(state, payload) {
-    state.isLogin = true;
-    state.loginUser.userNo = payload.data.userNo;
-    state.loginUser.userNickname = payload.data.userNickname;
-    state.loginUser.userGrade = payload.data.userGrade;
-  },
-  //로그아웃
-  USER_LOGOUT(state) {
-    state.isLogin = false;
-    state.loginUser = {
-      userNo: "",
-      userNickname: "",
-      userGrade: ""
-    }
-  },
-  //쿠키에 로그인 정보 저장
-  SAVE_LOGIN_INFO(state) {
-    VueCookies.set("login.userNo", state.loginUser.userNo, '30m');
-    VueCookies.set("login.userNickname", state.loginUser.userNickname, '30m');
-    VueCookies.set("login.userGrade", state.loginUser.userGrade, '30m');
-    VueCookies.set("isLogin", state.isLogin, '30m');
-  },
-  //쿠키에 있는 로그인 정보 읽어오기
-  GET_LOGIN_INFO(state) {
-    if (VueCookies.get("login.userNo") != null) {
-      state.loginUser.userNo = VueCookies.get("login.userNo")
-    }
-    if (VueCookies.get("login.userNickname") != null) {
-      state.loginUser.userNickname = VueCookies.get("login.userNickname")
-    }
-    if (VueCookies.get("login.userGrade") != null) {
-      state.loginUser.userGrade = VueCookies.get("login.userGrade")
-    }
-    if (VueCookies.get("isLogin") != null) {
-      state.isLogin = VueCookies.get("isLogin")
-    }
-  },
-  //쿠키에 있는 로그인 정보 삭제
-  RESET_LOGIN_INFO() {
-    VueCookies.remove("login.userNo");
-    VueCookies.remove("login.userNickname");
-    VueCookies.remove("login.userGrade");
-    VueCookies.remove("isLogin");
-  },
 };
 
 export default {
--- ssauction-front/src/views/Login.vue
+++ ssauction-front/src/views/Login.vue
@@ -1,121 +1,13 @@
 <template>
   <div>
-    <br />
-    <h1>로그인</h1>
-    <br />
-    <!-- <label for="user_email">이메일: </label> -->
-    <input
-      id="user_email"
-      type="email"
-      v-model="loginInfo.userEmail"
-      placeholder="이메일"
-    />
-    <br />
-    <br />
-    <!-- <label for="user_pwd">비밀번호: </label> -->
-    <input
-      id="user_pwd"
-      type="password"
-      v-model="loginInfo.userPwd"
-      placeholder="비밀번호"
-    />
-    <br />
-    <br />
-    <button @click="login" width="500px" class="btn_yellow">로그인</button>
-    <br />
-    <br />
-    <button @click="findid()" class="btn_pink">아이디 찾기</button>&nbsp;
-    <button @click="resetpwd()" class="btn_pink">비밀번호 재설정</button>&nbsp;
-    <br />
-    <br />
-    <br />
-    <a href="/oauth2/authorization/google"
-      ><img
-        src="@\assets\imgs\google.png"
-        alt="구글 로그인"
-        class="logo_resize" /></a
-    >&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-    <a href=""
-      ><img
-        src="@\assets\imgs\naver.png"
-        alt="네이버 로그인"
-        class="logo_resize"
-    /></a>
-    <!-- <br>
-    <button class="btn_pink">구글로 회원가입</button> &nbsp;
-    <button class="btn_pink">네이버로 회원가입</button> &nbsp; -->
+
   </div>
 </template>
 
 <script>
-// import router from '@/router'
 
-export default {
-  name: "SsauctionLogin",
-
-  data() {
-    return {
-      //로그인 객체
-      loginInfo: {
-        userEmail: "",
-        userPwd: "",
-      },
-    };
-  },
-
-  mounted() {},
-
-  methods: {
-    login() {
-      console.log(this.loginInfo);
-      //user 모듈의 userLogin으로 보낸다.
-      this.$store.dispatch("user/userLogin", this.loginInfo);
-    },
-  },
-};
 </script>
 
 <style>
-/* h1 {
-  margin-left:110px
-} */
 
-input {
-  width: 500px;
-  height: 50px;
-  border: 0;
-  background-color: rgb(230, 230, 230);
-  border-radius: 20px;
-  color: rgb(94, 94, 94);
-  padding-left: 30px;
-}
-
-input:focus {
-  outline: 3px rgb(94, 94, 94);
-}
-
-.btn_pink {
-  width: 245px;
-  height: 50px;
-  border: 0;
-  background-color: rgb(255, 170, 165);
-  border-radius: 20px;
-  color: rgb(94, 94, 94);
-  text-align: center;
-}
-
-.btn_yellow {
-  width: 500px;
-  height: 50px;
-  border: 0;
-  background-color: rgb(255, 211, 182);
-  border-radius: 20px;
-  color: rgb(94, 94, 94);
-  text-align: center;
-}
-
-.logo_resize {
-  width: 60px;
-  height: 60px;
-}
 </style>
