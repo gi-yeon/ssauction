db2df0ada8917664385ef6fe190fa61c6b5979fe
김태하 <rlaxogk006@naver.com>
2022-08-18 16:51:32 +0900

    Update ssauction_back/src/main/java/com/ssafy/ssauction/config/WebConfig.java

--- ssauction_back/src/main/java/com/ssafy/ssauction/config/WebConfig.java
+++ ssauction_back/src/main/java/com/ssafy/ssauction/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOrigins("http://52.78.53.155/","http://52.78.53.155:8080/")
+                .allowedOrigins("https://i7a806.p.ssafy.io/","https://i7a806.p.ssafy.io/ssauction")
                 .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE", "HEAD")
                 .allowedHeaders("*");
     }
