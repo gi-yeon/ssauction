12d677807d74d15434b886e0f6fe1c0121f1f560
김태하 <rlaxogk006@naver.com>
2022-08-18 14:31:49 +0900

    Update ssauction-front/vue.config.js

--- ssauction-front/vue.config.js
+++ ssauction-front/vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
+  publicPath: '',
   // devServer: {
   //   port: 8083,
   //   proxy: "http://localhost:8080",
