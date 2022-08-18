3230b7358a8a72b425e4d0ccfb2c1d9517847425
김태하 <rlaxogk006@naver.com>
2022-08-18 16:42:06 +0900

    Update ssauction-front/src/utils/axios.js

--- ssauction-front/src/utils/axios.js
+++ ssauction-front/src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 axios.defaults.headers.post["Content-Type"] = "application/json";
 axios.defaults.headers.put["Content-Type"] = "application/json";
-axios.defaults.baseURL = 'https://52.78.53.155:8080/ssauction';
+axios.defaults.baseURL = 'https://i7a806.p.ssafy.io/ssauction';
 export default axios;
 
